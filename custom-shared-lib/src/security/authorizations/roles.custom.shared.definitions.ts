c82089ec38c4e3f240d1f8e6e949905f94307a8a
Paul Cayon <paul.cayon@gmail.com>
2025-12-22 05:52:46 +0100

    gf

--- custom-shared-lib/src/security/authorizations/roles.custom.shared.definitions.ts
+++ custom-shared-lib/src/security/authorizations/roles.custom.shared.definitions.ts
@@ -1,15 +1,15 @@
-import { RolesConfiguration, ResourcePrimaryScope } from "@wildo-ai/saas-models";
+import { RolesConfiguration, ResourcePrimaryScope, CORE_APP_ROLES, CORE_ORG_ROLES } from "@wildo-ai/saas-models";
 
 export const CUSTOM_ROLES_CONFIGURATION: RolesConfiguration = {
   'CUSTOM_APP_MANAGER': {
     role: 'CUSTOM_APP_MANAGER',
-    inheritFrom: 'APP_ADMIN_USER',
+    inheritFrom: CORE_APP_ROLES.APP_USER,
     isSystemRole: false,
     relatedPrimaryScope: ResourcePrimaryScope.APPLICATION
   },
   'CUSTOM_ORG_SUPERVISOR': {
     role: 'CUSTOM_ORG_SUPERVISOR', 
-    inheritFrom: 'ORG_MANAGER',
+    inheritFrom: CORE_ORG_ROLES.ORG_MANAGER,
     isSystemRole: false,
     relatedPrimaryScope: ResourcePrimaryScope.ORGANIZATIONS
   }
